b5e6f9295eeb1be314a54f6395a65e49441772ae
sroffey-github <82031457+sroffey-github@users.noreply.github.com>
2022-01-25 15:55:57 +0000

    Delete monitor.sh

--- monitor.sh
+++
@@ -1,7 +0,0 @@
-website=$1
-
-if curl -s --head  --request GET $website | grep "200 OK" > /dev/null; then 
-   sqlite3 db/info.db "UPDATE Monitors SET Status='Up' WHERE Link='$1';"
-else
-    sqlite3 db/info.db "UPDATE Monitors SET Status='Down' WHERE Link='$1';"
-fi
